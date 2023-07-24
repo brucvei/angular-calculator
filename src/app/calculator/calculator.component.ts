d7cf82d95b4b763e3e9360dce37defde2c5ed132
bruna <burna.caetano@acad.ufsm.br>
2023-07-24 11:35:12 -0300

    wip

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,9 +7,37 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  result = '0';
+  value: { active: boolean, value: string } = {active: true, value: '0'};
+
+  NUMBERS = '0123456789';
+  EQUAL = '=';
+
+  buttons: string[][] = [
+    ['AC', 'D', '%', '/'],
+    ['7', '8', '9', 'x'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['Theme', '0', ',', this.EQUAL]
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  buttonColor(button: string): string {
+    if (!this.NUMBERS.includes(button)) {
+      switch (button) {
+        case ',':
+          return '';
+        case this.EQUAL:
+          return 'equal-button';
+        default:
+          return 'other-button';
+      }
+    }
+    return '';
+  }
 }
